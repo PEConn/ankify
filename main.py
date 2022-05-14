ac46d530d060f92eff32c143bf8641c1255c7ed6
PEConn <conn.peter@gmail.com>
2022-05-14 06:55:06 +0000

    Set up env.

---
+++ main.py
@@ -0,0 +1,10 @@
+from genanki import Note, CLOZE_MODEL
+
+if __name__ == "__main__":
+    note = Note(
+        model=CLOZE_MODEL,
+        fields=[
+            '{{c1::Rome}} is the capital of {{c2::Italy}}', ''
+        ])
+
+    print(note)
