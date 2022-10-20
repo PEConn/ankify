71e4cba12cbc4028c825a606e18496a4b2133336
PEConn <conn.peter@gmail.com>
2022-10-20 07:12:50 +0100

    Basic card parsing.

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
 from genanki import Note, CLOZE_MODEL
 
+from dataclasses import dataclass
+
+# TODO: Clean up my terminology.
+@dataclass
+class Card:
+    """Keeps the result of parsing a single card."""
+    contents: str
+
+def parse(contents):
+    paragraphs = contents.split("\n\n")
+    cards = []
+    next_is_card = False
+
+    for paragraph in paragraphs:
+        paragraph = paragraph.strip()
+        if next_is_card:
+            cards += [paragraph]
+            next_is_card = False
+        elif paragraph == "<!-- cloze -->":
+            next_is_card = True
+
+    return [parse_card(card) for card in cards]
+
+def parse_card(card):
+    # card = "The capital of ==France== is ==Paris==."
+    # 1. Split on ==.
+    # 2. Join with ['{{c1::', '}}', '{{c2::', '}}', ..]
+    parts = card.split("==")
+    assert len(parts) % 2 == 1
+    # TODO: What about cards that end with a ==?
+
+    result = []
+    s = separators()
+    for part in parts:
+        result += [part, next(s)]
+
+    return Card(contents="".join(result[:-1]))
+
+def separators():
+    num = 1
+    while True:
+        yield f"{{{{c{num}::"
+        yield "}}"
+        num += 1
+
 if __name__ == "__main__":
     note = Note(
         model=CLOZE_MODEL,
--- main_test.py
+++ main_test.py
@@ -1,2 +1,53 @@
-def test_thing():
-  assert 2 == 1
+from main import Card, parse, parse_card, separators
+
+SINGLE_CARD = """
+some text
+
+<!-- cloze -->
+
+The capital of ==France== is ==Paris==.
+
+some other text.
+"""
+
+MULTI_CARD = """
+<!-- cloze -->
+
+The capital of ==France== is ==Paris==.
+
+<!-- cloze -->
+
+The capital of ==Germany== is ==Berlin==.
+"""
+
+def test_parse_num_results_single():
+    assert 1 == len(parse(SINGLE_CARD))
+
+def test_parse_num_results_multiple():
+    assert 2 == len(parse(MULTI_CARD))
+
+def test_parse_single():
+    exp = [Card(contents = "The capital of {{c1::France}} is {{c2::Paris}}.")]
+
+    assert exp == parse(SINGLE_CARD)
+
+def test_parse_multiple():
+    exp = [
+        Card(contents = "The capital of {{c1::France}} is {{c2::Paris}}."),
+        Card(contents = "The capital of {{c1::Germany}} is {{c2::Berlin}}.")
+    ]
+
+    assert exp == parse(MULTI_CARD)
+
+def test_separators():
+    s = separators()
+    assert "{{c1::" == next(s)
+    assert "}}" == next(s)
+    assert "{{c2::" == next(s)
+    assert "}}" == next(s)
+
+def test_parse_card():
+    card = "The capital of ==France== is ==Paris==."
+    exp = Card(contents = "The capital of {{c1::France}} is {{c2::Paris}}.")
+
+    assert exp == parse_card(card)
