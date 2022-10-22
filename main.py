d6ca1c46490a6680d7f126c73dd7ea3ae1c3cac2
PEConn <conn.peter@gmail.com>
2022-10-22 07:57:08 +0100

    Create an output file.

--- main.py
+++ main.py
@@ -27,36 +27,61 @@ def random_generator() -> GuidGenerator:
         yield randrange(MAX)
 
 
+@dataclass
+class ParseResult:
+    """Holds the list of cards and the updated file as a result of a parse."""
+
+    cards: List[Card]
+    updated_file: str
+
+
 def parse(
     contents: str, guid_generator: GuidGenerator = random_generator()
-) -> List[Card]:
+) -> ParseResult:
     paragraphs = contents.split("\n\n")
     cards: List[Card] = []
     cloze_for_next: Optional[ClozeTag] = None
+    updated_file: List[str] = []
 
     for paragraph in paragraphs:
-        paragraph = paragraph.strip()
+        if cloze_for_next:
+            cards += [parse_card(paragraph, cloze_for_next.guid)]
+
+        cloze_for_next = parse_cloze_tag(paragraph, guid_generator)
 
         if cloze_for_next:
-            if cloze_for_next.guid is None:
-                guid = next(guid_generator)
-            else:
-                guid = cloze_for_next.guid
-            cards += [parse_card(paragraph, guid)]
+            updated_file += [cloze_for_next.to_string()]
+        else:
+            updated_file += [paragraph]
 
-        cloze_for_next = parse_cloze_tag(paragraph)
-
-    return cards
+    return ParseResult(cards, updated_file="\n\n".join(updated_file))
 
 
 @dataclass
 class ClozeTag:
-    guid: Optional[int]
+    """Holds information about the <!-- cloze --> tag, used to signify a card is coming next."""
+
+    guid: int
+
+    # The leading and trailing whitespace the cloze originally had.
+    # We need this to preserve it for the output.
+    prefix: str = ""
+    suffix: str = ""
+
+    def to_string(self):
+        return f"{self.prefix}<!-- cloze id:{self.guid} -->{self.suffix}"
 
 
-def parse_cloze_tag(line: str) -> Optional[ClozeTag]:
+def parse_cloze_tag(line: str, guid_generator: GuidGenerator) -> Optional[ClozeTag]:
+    unstripped_line = line
+    line = line.strip()
+
     if not line.startswith("<!--") or not line.endswith("-->"):
         return None
+
+    # If the cloze has some leading or trailing whitespace, we want to preserve it for the output.
+    prefix = unstripped_line[: unstripped_line.find("<")]
+    suffix = unstripped_line[unstripped_line.find(">") + 1 :]
 
     parts = line.split(" ")
 
@@ -67,16 +92,17 @@ def parse_cloze_tag(line: str) -> Optional[ClozeTag]:
         if not part.startswith("id:"):
             continue
         try:
-            return ClozeTag(guid=int(part[len("id:") :]))
+            return ClozeTag(guid=int(part[len("id:") :]), prefix=prefix, suffix=suffix)
         except ValueError:
             pass
 
-    return ClozeTag(guid=None)
+    return ClozeTag(guid=next(guid_generator), prefix=prefix, suffix=suffix)
 
 
 # TODO: HTML encode contents:
 # https://github.com/kerrickstaley/genanki#my-field-data-is-getting-garbled
 def parse_card(card: str, guid: int) -> Card:
+    card = card.strip()
     # card = "The capital of ==France== is ==Paris==."
     # 1. Split on ==.
     # 2. Join with ['{{c1::', '}}', '{{c2::', '}}', ..]
@@ -92,7 +118,7 @@ def parse_card(card: str, guid: int) -> Card:
     return Card(contents="".join(result[:-1]), guid=guid)
 
 
-def separators():
+def separators() -> Generator[str, None, None]:
     num = 1
     while True:
         yield f"{{{{c{num}::"
--- main_test.py
+++ main_test.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 from main import (
     Card,
     ClozeTag,
@@ -37,17 +39,33 @@ def counting_generator() -> GuidGenerator:
 
 
 def test_parse_num_results_single():
-    assert 1 == len(parse(SINGLE_CARD))
+    assert 1 == len(parse(SINGLE_CARD).cards)
 
 
 def test_parse_num_results_multiple():
-    assert 2 == len(parse(MULTI_CARD))
+    assert 2 == len(parse(MULTI_CARD).cards)
 
 
 def test_parse_single():
     exp = [Card(contents="The capital of {{c1::France}} is {{c2::Paris}}.", guid=0)]
 
-    assert exp == parse(SINGLE_CARD, guid_generator=counting_generator())
+    assert exp == parse(SINGLE_CARD, guid_generator=counting_generator()).cards
+
+
+def test_parse_single_updated_file():
+    exp = dedent(
+        """
+    some text
+
+    <!-- cloze id:0 -->
+
+    The capital of ==France== is ==Paris==.
+
+    some other text.
+    """
+    )
+
+    assert exp == parse(SINGLE_CARD, guid_generator=counting_generator()).updated_file
 
 
 def test_parse_multiple():
@@ -56,7 +74,23 @@ def test_parse_multiple():
         Card(contents="The capital of {{c1::Germany}} is {{c2::Berlin}}.", guid=0),
     ]
 
-    assert exp == parse(MULTI_CARD, guid_generator=counting_generator())
+    assert exp == parse(MULTI_CARD, guid_generator=counting_generator()).cards
+
+
+def test_parse_multiple_updated_file():
+    exp = dedent(
+        """
+    <!-- cloze id:123 -->
+
+    The capital of ==France== is ==Paris==.
+
+    <!-- cloze id:0 -->
+
+    The capital of ==Germany== is ==Berlin==.
+    """
+    )
+
+    assert exp == parse(MULTI_CARD, guid_generator=counting_generator()).updated_file
 
 
 def test_separators():
@@ -75,20 +109,32 @@ def test_parse_card():
 
 
 def test_parse_cloze_tag_no_id():
-    assert ClozeTag(guid=None) == parse_cloze_tag("<!-- cloze -->")
+    assert ClozeTag(guid=0) == parse_cloze_tag("<!-- cloze -->", counting_generator())
 
 
 def test_parse_cloze_tag_invalid_no_keyword():
-    assert None is parse_cloze_tag("<!-- -->")
+    assert None is parse_cloze_tag("<!-- -->", counting_generator())
 
 
 def test_parse_cloze_tag_with_id():
-    assert ClozeTag(guid=123) == parse_cloze_tag("<!-- cloze id:123 -->")
+    assert ClozeTag(guid=123) == parse_cloze_tag(
+        "<!-- cloze id:123 -->", counting_generator()
+    )
 
 
 def test_parse_cloze_tag_with_invalid_id():
-    assert ClozeTag(guid=None) == parse_cloze_tag("<!-- cloze id:apple -->")
+    assert ClozeTag(guid=0) == parse_cloze_tag(
+        "<!-- cloze id:apple -->", counting_generator()
+    )
 
 
 def test_parse_cloze_tag_invalid():
-    assert None is parse_cloze_tag("The capital of ==France== is ==Paris==.")
+    assert None is parse_cloze_tag(
+        "The capital of ==France== is ==Paris==.", counting_generator()
+    )
+
+
+def test_parse_cloze_tag_preserve_whitespace():
+    assert ClozeTag(guid=123, prefix=" ", suffix="  ") == parse_cloze_tag(
+        " <!-- cloze id:123 -->  ", counting_generator()
+    )
